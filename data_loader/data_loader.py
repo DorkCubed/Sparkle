d08c191c896a187d8d9d0dcd6f424085675816b1
marslayr <hephannn@gmail.com>
2025-08-16 00:26:17 +0530

    minor bug fix in data_module. TODO: test data loader

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -3,11 +3,14 @@ from torch.utils.data import DataLoader
 from .dataset import PacketSequenceDataset
 from .config import Config
 from .tokenizer.tokenizer import Tokenizer
+from scripts.s3_utils import S3DataFetcher
 
 class DataModule:
     def __init__(self):
         self.config = Config()
         self.tokenizer = self._load_tokenizer()
+        self.s3_fetcher = S3DataFetcher(bucket_name=self.config.bucket_name)
+        self.files = self.s3_fetcher.list_split_objects(self.config.parents, self.config.split)
         self.dataset = self._init_dataset()
         self.train_loader = self._init_dataloader()
 
@@ -15,12 +18,18 @@ class DataModule:
         return Tokenizer(vocab_file=self.config.tokenizer_path)
 
     def _init_dataset(self):
-        return PacketSequenceDataset(self.config.packet_folder, self.config.header_folder, self.config.fields_folder, self.tokenizer, chunk_size=self.config.chunk_size)
+        return PacketSequenceDataset(
+            config=self.config,
+            s3_fetcher=self.s3_fetcher,
+            files=self.files,
+            tokenizer=self.tokenizer,
+            chunk_size=self.config.chunk_size
+        )
 
     def _init_dataloader(self):
-        return DataLoader(self.dataset, batch_size=self.config.chunk_size, shuffle=False)
+        return DataLoader(self.dataset, batch_size=self.config.batch_size, shuffle=True)
 
-    def _get_loader(self):
+    def get_loader(self):
         return self.train_loader
 
 
@@ -32,34 +41,18 @@ def test_data_loader(data_loader, num_batches=2):
         print(f"Header Position shape: {header_position.shape}")
         print(f"File Name: {packet_seq_file_name}")
 
-        # Optional: Inspect first few token IDs
         print("First packet sequence tokens:", packet_sequences[0, :50])
 
-        # Sanity checks
-        assert isinstance(packet_sequences, torch.Tensor), "packet_sequences is not a tensor"
-        assert isinstance(field_position, torch.Tensor), "field_position is not a tensor"
-        assert isinstance(header_position, torch.Tensor), "header_position is not a tensor"
+        assert isinstance(packet_sequences, torch.Tensor)
+        assert isinstance(field_position, torch.Tensor)
+        assert isinstance(header_position, torch.Tensor)
 
         if i + 1 >= num_batches:
             break
 
 
-
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     data_module = DataModule()
-
-    config = data_module.config
-
-    tokenizer = data_module.tokenizer
-
-    dataset = PacketSequenceDataset(config=config, s3_fetcher=config.fetcher, files=config.files, tokenizer=tokenizer, chunk_size=config.chunk_size)
-    train_loader = DataLoader(dataset, batch_size=1, shuffle=False)
-
+    train_loader = data_module.get_loader()
     test_data_loader(train_loader, 3)
-
-
-
-
-
-
