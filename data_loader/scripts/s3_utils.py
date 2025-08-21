65cee47db374bedb1fe7da53fa388a1b94141e3a
Atharva Gupta <hephannn@gmail.com>
2025-08-21 13:55:56 +0530

    script to build manifest

--- data_loader/scripts/s3_utils.py
+++ data_loader/scripts/s3_utils.py
@@ -1,4 +1,14 @@
 import s3fs
+import os
+import json
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+logger = logging.getLogger(__name__)
+
 
 class S3DataFetcher:
     def __init__(self, bucket_name):
@@ -32,9 +42,53 @@ class S3DataFetcher:
                 files.append((p, h, f, d))
         return files
 
+    def build_manifest(self, parent):
+        manifest = []
+
+        flows = self.fs.ls(parent)
+
+        logger.info(f"Found {len(flows)} flow directories under {parent}")
+
+        for i, flow in enumerate(flows):
+            if not self.fs.isdir(flow):
+                continue
+
+            logger.info(f"[{i}/{len(flows)}] Processing flow: {flow}")
+
+            packets = self.fs.glob(f"{flow}/packets/*.txt")
+            headers = self.fs.glob(f"{flow}/header/*.txt")
+            fields = self.fs.glob(f"{flow}/fields/*.txt")
+            directions = self.fs.glob(f"{flow}/direction/*.txt")
+
+
+            for p, h, f, d in zip(sorted(packets), sorted(headers), sorted(fields), sorted(directions)):
+                manifest.append({
+                    "flow": os.path.basename(flow),
+                    "packet": p,
+                    "header": h,
+                    "field": f,
+                    "direction": d
+                })
+
+        return manifest
+
+    def save_manifest_to_json(self, manifest, output_path):
+        with open(output_path, 'w') as f:
+            json.dump(manifest, f, indent=4)
+
+        print(f"Manifest saved to {output_path}")
+
     def read_files(self, s3_path):
         """
         Reads the contents of a file from an S3 path.
         """
         with self.fs.open(s3_path, 'r') as f:
             return f.read()
+
+
+if __name__ == "__main__":
+    bucket_name = "netml-s3-bucket"
+    folder = 'Working_folder/input_aws/Wireshark_Sample_PCAPs/split'
+    fetcher = S3DataFetcher(bucket_name)
+    manifest = fetcher.build_manifest(parent=f"s3://{bucket_name}/{folder}")
+    fetcher.save_manifest_to_json(manifest, 'manifest.json')
