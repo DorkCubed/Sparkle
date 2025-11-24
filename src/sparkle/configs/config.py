f8227090aea6b9b393f2cada0bf94620ba50e5b9
Atharva Gupta <hephannn@gmail.com>
2025-11-25 00:22:51 +0530

    change manifest path.

--- src/sparkle/configs/config.py
+++ src/sparkle/configs/config.py
@@ -20,7 +20,8 @@ class Config:
         self.tokenizer_path = os.path.join(parent_dir, "data_loader", "tokenizer", "vocab.txt")
         # self.manifest_path = os.path.join(parent_dir, "manifest", "manifest.json")
         # self.manifest_path = os.path.join(parent_dir, "manifest", "test_manifest.json")
-        self.manifest_path = os.path.join(project_root, "manifest", "manifest.json")
+        # self.manifest_path = os.path.join(project_root, "manifest", "manifest.json")
+        self.manifest_path = os.path.join(project_root, "manifest", "manifest_1000.json")
         self.batch_size = 1
         self.shuffle = False
 
